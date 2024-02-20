0be897814c528affffe4e67adf153b19db7222a2
thaleseqb <thalesquadros2@gmail.com>
2024-02-20 11:30:04 -0300

    automatizate function that changes the images through click

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const html = document.querySelector('html')
-const focus_button = document.querySelector('.app__card-button--foco')
-const short_rest = document.querySelector('.app__card-button--curto')
-const long_rest = document.querySelector('.app__card-button--longo')
+const html = document.querySelector('html');
+const focus_button = document.querySelector('.app__card-button--foco');
+const short_rest = document.querySelector('.app__card-button--curto');
+const long_rest = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
-focus_button.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-})
+let list_1 = [focus_button, short_rest, long_rest];
+let list_2 = ['foco', 'descanso-curto', 'descanso-longo'];
 
-short_rest.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-})
+function change_context(button, list, list_strng){
+    if (list.includes(button)) {
+        const index = list.indexOf(button);
+        button.addEventListener('click', () => {
+            html.setAttribute('data-contexto', list_strng[index]);
+            banner.setAttribute('src', `/assets/${list_strng[index]}.png`);
+        });
+    };
+};
 
-long_rest.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-})
+change_context(short_rest, list_1, list_2)
+change_context(focus_button, list_1, list_2)
+change_context(long_rest, list_1, list_2)
