526e638281155296d483889b1e3d1c8a8cf617f9
thaleseqb <thalesquadros2@gmail.com>
2024-02-21 11:25:06 -0300

    add tasks in forms preventing the default behavinf of the site and storaging tasks in an array by the aplication of localstorage

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,17 @@
 const button_add_task = document.querySelector('.app__button--add-task');
-const button_form = document.querySelector('.app__form-add-task');
+const add_form_task = document.querySelector('.app__form-add-task');
+const text_area = document.querySelector('.app__form-textarea');
+const tasks = [];
 
 button_add_task.addEventListener('click', () => {
-    button_form.classList.toggle('hidden');
-})
+    add_form_task.classList.toggle('hidden');
+});
+
+add_form_task.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const task = {
+        description: text_area.value
+    };
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+});
